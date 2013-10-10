c92de2441367444bae8981d401b4594d59229811
Sudip iPhone <citytech@Sudip-iPhone.local>
2013-10-10 11:55:16 +0530

    Done some stuff on login class.

--- TestRaj/LoginViewController.m
+++ TestRaj/LoginViewController.m
@@ -27,6 +27,9 @@
 {
     [super viewDidLoad];
     // Do any additional setup after loading the view from its nib.
+    
+    
+    NSLog(@"This is second day of my git repository. Able to reach Login");
 }
 
 - (void)didReceiveMemoryWarning
